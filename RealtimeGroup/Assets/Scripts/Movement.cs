aefdb7bc91fbd683762e4771aed97b805c4ef38f
EricMcCaw <40258078+EricMcCaw@users.noreply.github.com>
2019-12-03 14:02:02 -0500

    figured out multiplayer inputs

--- RealtimeGroup/Assets/Scripts/Movement.cs
+++ RealtimeGroup/Assets/Scripts/Movement.cs
@@ -6,19 +6,31 @@ public class Movement : MonoBehaviour
 {
     public float movementSp;
     protected Rigidbody Rb;
+    bool CanJump;
+    int Playernum;
     // Start is called before the first frame update
     void Start()
     {
-       
+        CanJump = true;
         Rb = transform.GetComponent<Rigidbody>();
+        Playernum = 3;
     }
 
     // Update is called once per frame
     void Update()
     {
         GetInputs();
+        Jump();
     }
 
+    public void Jump()
+    {
+        if(Input.GetAxis("Jump"+ Playernum) != 0 && CanJump)
+        {
+            Rb.AddForce(new Vector3(0,800,0));
+            CanJump = false;
+        }
+    }
     public void GetInputs(float x = 0, float z = 0)
     {
         Vector3 dz;
@@ -26,7 +38,6 @@ public class Movement : MonoBehaviour
         if (x == 0 && z == 0)
         {
             dz = Input.GetAxis("Horizontal") * transform.forward;
-            
         }
         else
         {
@@ -35,10 +46,27 @@ public class Movement : MonoBehaviour
         Vector3 inputs = dz;
 
         //moving using rb still checks collisions without feeling like a slip n slide 
-        Vector3 movement = inputs.normalized * movementSp * Time.deltaTime;
-        Rb.AddForce(movement);
-        //Rb.MovePosition(movement);
+        if (!CanJump)
+        {
+            Vector3 movement = inputs.normalized * movementSp / 4 * Time.deltaTime;
+            Rb.AddForce(movement);
+        }
+        else
+        {
+            Vector3 movement = inputs.normalized * movementSp * Time.deltaTime;
+            Rb.AddForce(movement);
+        }
 
 
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Platform")
+        {
+            CanJump = true;
+        }
+    }
+
+
 }
