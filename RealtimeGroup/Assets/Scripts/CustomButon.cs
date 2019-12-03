b77c3a8890f95f4c26e6d845627bf926a5d20d7a
Alex <kjakarproductions@gmail.com>
2019-12-03 15:46:39 -0500

    Added main menu base
    Added a simple main menu, havn't added the name input yet.

---
+++ RealtimeGroup/Assets/Scripts/CustomButon.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class CustomButon : MonoBehaviour
+{
+    private bool hover = false;
+
+    private float oScaleX, oScaleY;
+
+    private void Start()
+    {
+        oScaleX = transform.localScale.x;
+        oScaleY = transform.localScale.y;
+    }
+
+
+    void Update()
+    {
+        if (hover)
+        {
+            print("hi");
+            if (transform.localScale.x <= oScaleX * 1.5)
+            {
+                double x = transform.localScale.x + transform.localScale.x * Time.deltaTime * 2;
+                double y = transform.localScale.y + transform.localScale.y * Time.deltaTime * 2;
+                transform.localScale = new Vector3((float)x, (float)y,1);
+            }
+        }
+        else
+        {
+
+            if (transform.localScale.x >= oScaleX)
+            {
+                double x = transform.localScale.x - transform.localScale.x * Time.deltaTime;
+                double y = transform.localScale.y - transform.localScale.y * Time.deltaTime;
+                transform.localScale = new Vector3((float)x, (float)y, 1);
+            }
+        }
+    }
+
+    public void isMouseOver(bool b)
+    {
+        hover = b;
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
