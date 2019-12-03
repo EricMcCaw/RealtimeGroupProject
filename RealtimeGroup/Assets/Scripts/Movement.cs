e857180d988b323374f2e806450076ef5804c8b5
EricMcCaw <40258078+EricMcCaw@users.noreply.github.com>
2019-12-03 13:05:16 -0500

    Player Movement

--- RealtimeGroup/Assets/Scripts/Movement.cs
+++ RealtimeGroup/Assets/Scripts/Movement.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    public float movementSp;
+    protected Rigidbody Rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
+        Rb = transform.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position= new Vector3(transform.position.x,transform.position.y , transform.position.z+ Input.GetAxis("Horizontal"));
+        GetInputs();
+    }
+
+    public void GetInputs(float x = 0, float z = 0)
+    {
+        Vector3 dz;
+
+        if (x == 0 && z == 0)
+        {
+            dz = Input.GetAxis("Horizontal") * transform.forward;
+            
+        }
+        else
+        {
+            dz = x * transform.forward;
+        }
+        Vector3 inputs = dz;
+
+        //moving using rb still checks collisions without feeling like a slip n slide 
+        Vector3 movement = inputs.normalized * movementSp * Time.deltaTime;
+        Rb.AddForce(movement);
+        //Rb.MovePosition(movement);
+
+
     }
 }
