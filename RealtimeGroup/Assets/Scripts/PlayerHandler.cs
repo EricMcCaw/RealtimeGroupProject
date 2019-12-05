de812599d0cef52491bd056b97ad2bfc58523b19
EricMcCaw <40258078+EricMcCaw@users.noreply.github.com>
2019-12-05 13:09:45 -0500

    player handler script

---
+++ RealtimeGroup/Assets/Scripts/PlayerHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHandler : MonoBehaviour
+{
+    public GameObject Playerprefab;
+    GameObject Player1;
+    GameObject Player2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player1 = Instantiate(Playerprefab,transform.position + new Vector3(0,0,0), Quaternion.Euler(0, 90, 0));
+        Player2 = Instantiate(Playerprefab,transform.position + new Vector3(2,0,0), Quaternion.Euler(0,90,0));
+
+        Player2.GetComponent<Movement>().Playernum = 2;
+        Player1.GetComponent<Movement>().Playernum = 1;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
