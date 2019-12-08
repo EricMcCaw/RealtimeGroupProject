681a5fb95549d8ba2a29130b0ecd345d0e035016
Alex <kjakarproductions@gmail.com>
2019-12-08 16:35:51 -0500

    Added a score and timer UI
    Added a score and timer UI the score uses the names the player input in the main menu

---
+++ RealtimeGroup/Assets/Scripts/ScoreUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreUI : MonoBehaviour
+{
+    public Text timerText, player1NameText, player2NameText;
+    private string player1Name, player2Name;
+    private float timer = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player1Name = StaticClass.playerOneName + " : ";
+        player2Name = StaticClass.playerTwoName + " : ";
+
+        timerText.text = "00:00";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        int min = Mathf.FloorToInt(timer / 60);
+        int sec = Mathf.FloorToInt(timer % 60);
+        timerText.text = min.ToString("00") + ":" + sec.ToString("00");
+
+        player1NameText.text = player1Name + StaticClass.playerOneScore.ToString();
+        player2NameText.text = player2Name + StaticClass.playerTwoScore.ToString();
+    }
+}
